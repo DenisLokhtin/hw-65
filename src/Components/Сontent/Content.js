b9e3ecbed1e2cd4488663ff3298ceb77d512ae17
Lokhtin Denis <denis.lokhtin@gmail.com>
2021-09-02 15:34:24 +0600

    получение и вывод информации работает

--- src/Components/Сontent/Content.js
+++ src/Components/Сontent/Content.js
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './Content.css'
+import axiosApi from "../../AxiosApi";
 
-const Content = (props) => (
-    <div>
-        <h2>Title</h2>
-        <p>Text</p>
-    </div>
-);
+const Content = (props) => {
+    let CurrentURL = props.history.location.pathname;
+    const [text, setText] = useState('');
+    const [title, setTitle] = useState('');
+
+    const getInfo = async () => {
+        try {
+            if (CurrentURL === '/') {
+                await axiosApi.get('home.json').then(response => {
+                    if (response.data !== null) {
+                        setText(response.data.text);
+                        setTitle(response.data.title);
+                    }
+                });
+            } else {
+                await axiosApi.get(`${CurrentURL}` + '.json').then(response => {
+                    if (response.data !== null) {
+                        setText(response.data.text);
+                        setTitle(response.data.title);
+                    }
+                })
+            }
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+    useEffect(() => {
+        getInfo()
+    }, [CurrentURL])
+
+    return (
+        <div>
+            <h2>{title}</h2>
+            <p>{text}</p>
+        </div>
+    )
+};
 
 export default Content;
